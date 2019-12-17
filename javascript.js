32c1d86affab8cf8b4482cd8f2efb12e03fb4704
stevski11 <54551988+stevski11@users.noreply.github.com>
2019-12-16 20:13:47 -0600

    change to jquery

--- javascript.js
+++ javascript.js
@@ -3,18 +3,18 @@ var num = 0
 function newNum() {
     num = Math.floor(Math.random() * 100)+1;
     console.log("generating new number")
-    document.getElementById("output").innerHTML = "Guess a number between 1 and 100 below";
-    document.getElementById("guessCountOut").innerHTML = "Guess Count: 0";
+    $('#output').text("Guess a number between 1 and 100 below");
+    $('#guessCountOut').text("Guess Count: 0");
     guessCount = 0
 }
 function hideSpin() {
-    document.getElementById("thinking").style.display = "none";
+    $('#thinking').hide()
     console.log("hiding spinner")
 }
 function guess2() {
     console.log("assessing guess")
     var higherLower = ""
-    var guess = document.getElementById("guessInput").value;
+    var guess = $('#guessInput').val();
     if (guess == num) {
         higherLower = "You win"
     } else if (guess > num) {
@@ -24,19 +24,19 @@ function guess2() {
         higherLower = "Higher"
     }
     guessCount += 1
-    document.getElementById("thinking").style.display = "none"
+    $('#thinking').hide()
     console.log("hiding spinner")
-    document.getElementById("guessButton").disabled = false;
-    document.getElementById("restartButton").disabled = false;
+    $('#guessButton').prop("disabled", false);
+    $('#restartButton').prop("disabled", false);
     console.log("enabling buttons")
-    document.getElementById("output").innerHTML = higherLower
-    document.getElementById("guessCountOut").innerHTML = "Guess Count: " + guessCount
+    $('#output').text(higherLower)
+    $('#guessCountOut').text("Guess Count: " + guessCount)
 }
 function guess() {
-    document.getElementById("guessButton").disabled = true;
-    document.getElementById("restartButton").disabled = true;
+    $('#guessButton').prop("disabled", true);
+    $('#restartButton').prop("disabled", true);
     console.log("disabling buttons")
-    document.getElementById("thinking").style.display = "block";
+    $('#thinking').show();
     console.log("showing spinner")
     setTimeout(guess2, 3000);  
 }
